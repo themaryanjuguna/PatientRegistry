4023acfe8a0e54e661c97b4780f5e404cec8d8c4
themaryanjuguna <themaryanjugunaagmail.com>
2022-05-31 19:01:22 +0300

    modify colours

---
+++ app/src/main/java/com/moringaschool/tukio/splashActivity.java
@@ -0,0 +1,28 @@
+package com.moringaschool.tukio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //calling main activity to load after loading screen
+                startActivity(new Intent(splashActivity.this,MainActivity.class));
+                finish(); //finish loading screen activity
+            }
+            //call mainActivity after 5 seconds
+        }, 1000);
+
+    }
+}
